1931799fd7bceef96da83bcba38d79b8641f26ce
Vredinka <root.faust@gmail.com>
2019-05-02 18:13:37 +0300

    vis-bind

--- visbind/MainForm.h
+++ visbind/MainForm.h
@@ -112,7 +112,7 @@ namespace visbind {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(141, 155);
+			this->ClientSize = System::Drawing::Size(127, 141);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
